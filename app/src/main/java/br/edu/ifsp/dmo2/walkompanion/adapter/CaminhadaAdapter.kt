d8ba38b895023244c1c971c9f13aaf1c5c574130
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-07-10 10:46:17 -0300

    Reformatando código.

--- app/src/main/java/br/edu/ifsp/dmo2/walkompanion/adapter/CaminhadaAdapter.kt
+++ app/src/main/java/br/edu/ifsp/dmo2/walkompanion/adapter/CaminhadaAdapter.kt
@@ -10,20 +10,23 @@ import br.edu.ifsp.dmo2.walkompanion.model.Caminhada
 
 class CaminhadaAdapter(private val caminhadas: Array<Caminhada>) :
     RecyclerView.Adapter<CaminhadaAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val txtDate : TextView = view.findViewById(R.id.text_date)
-        val txtDuration : TextView = view.findViewById(R.id.text_time)
-        val txtSteps : TextView = view.findViewById(R.id.text_steps)
-        val txtDistance : TextView = view.findViewById(R.id.text_distance)
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtDate: TextView = view.findViewById(R.id.text_date)
+        val txtDuration: TextView = view.findViewById(R.id.text_time)
+        val txtSteps: TextView = view.findViewById(R.id.text_steps)
+        val txtDistance: TextView = view.findViewById(R.id.text_distance)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.caminhada_item, parent, false)
         return ViewHolder(view)
     }
+
     override fun getItemCount(): Int {
         return caminhadas.size
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.txtDate.text = caminhadas[position].getInicio().toString()
         holder.txtDuration.text = caminhadas[position].getDuration().toString()
--- app/src/main/java/br/edu/ifsp/dmo2/walkompanion/model/Caminhada.kt
+++ app/src/main/java/br/edu/ifsp/dmo2/walkompanion/model/Caminhada.kt
@@ -3,35 +3,36 @@ package br.edu.ifsp.dmo2.walkompanion.model
 import com.google.firebase.Timestamp
 import kotlin.time.Duration
 
-class Caminhada (
-    private val inicio : Timestamp,
-    private val steps : Int,
-    private val maxHeight : Float,
-    private val minHeigt : Float,
-    private val duration : Duration,
-    private val aproxDistance: Float) {
+class Caminhada(
+    private val inicio: Timestamp,
+    private val steps: Int,
+    private val maxHeight: Float,
+    private val minHeigt: Float,
+    private val duration: Duration,
+    private val aproxDistance: Float
+) {
 
-    public fun getInicio() : Timestamp{
+    public fun getInicio(): Timestamp {
         return inicio
     }
 
-    public fun getSteps() : Int{
+    public fun getSteps(): Int {
         return steps
     }
 
-    public fun getMaxHeight() : Float{
+    public fun getMaxHeight(): Float {
         return maxHeight
     }
 
-    public fun getMinHeight() : Float{
+    public fun getMinHeight(): Float {
         return minHeigt
     }
 
-    public fun getDuration() : Duration{
+    public fun getDuration(): Duration {
         return duration
     }
 
-    public fun getAproxDistance() : Float{
+    public fun getAproxDistance(): Float {
         return aproxDistance
     }
 }
